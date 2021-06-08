c597532edb7f0053e43540137a699404124ca90d
Márton Nagymarczi <62276878+bigmarczi1998@users.noreply.github.com>
2021-06-08 13:55:09 +0200

    Delete Programozas_1/hazi directory

--- Programozas_1/hazi/4hazi/1feladat.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-number=np.random.randint(1,10,size=10)
-print(number)
-avg=np.average(number)
-print('Átlag:',avg)
-dist=np.abs(avg-number)
-print(dist)
--- Programozas_1/hazi/4hazi/3feladat.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-number=np.arange(1,300,3)
-print(number)
-db=0
-for i in range(number.size):
-    if (number[i])%(4)==0:
-        number[i]=-4
-        db+=1
-print(db)
